251897ce1a6bef55ebbafc132c1323709540f798
Hadi <hadi@Hadis-Mac.local>
2018-09-07 00:38:03 +0500

    Initial Commit

---
+++ Get Parking/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Get Parking
+//
+//  Created by Hadi on 06/09/2018.
+//
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        return true
+    }
+
+}
+
---
+++ Get Parking/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Get Parking
+//
+//  Created by Hadi on 06/09/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
